53633cc4adeeca6146cf466a6038fc7c0bb8046c
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2023-09-29 03:42:47 +0000

    Added keyboard controls to move the car

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const canvas = document.querySelector("#myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
+
+
+animate();
+
+function animate() {
+    canvas.height = window.innerHeight;
+    car.update();
+    car.draw(ctx);
+    ctx
+    requestAnimationFrame(animate);
+}
