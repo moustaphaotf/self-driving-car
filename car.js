de3092f9d37d15933584865985edb8464d0fe9ed
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2023-09-29 13:45:20 +0000

    Added a collision detection system Now the car will stop upon collision with the road borders Also, the method is generalized to work with any kind of obstacle Then, implemented a new way to draw the car...

--- car.js
+++ car.js
@@ -10,14 +10,19 @@ class Car{
         this.maxSpeed = 5;
         this.friction = 0.05;
         this.angle = 0;
+        this.damaged = false;
 
         this.sensors = new Sensors(this);
         this.controls = new Controls();
     }
 
     update(roadBorders) {
-        this.#move();
-        this.sensors.update(roadBorders);
+        if(!this.damaged) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders);
+            this.sensors.update(roadBorders);
+        }
     }
     
     #move() {
@@ -66,19 +71,50 @@ class Car{
     }
 
     draw(ctx) {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-
+        if(this.damaged) {
+            ctx.fillStyle = 'gray';
+        } else {
+            ctx.fillStyle = 'black';
+        }
         ctx.beginPath();
-        ctx.rect(
-            -this.width / 2,
-            -this.height / 2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for(let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
-        ctx.restore();
         this.sensors.draw(ctx);
+    }
+
+    #createPolygon(){
+        const points = [];
+        const radius = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * radius,
+            y: this.y - Math.cos(this.angle - alpha) * radius,
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * radius,
+            y: this.y - Math.cos(this.angle + alpha) * radius,
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * radius,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * radius,
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * radius,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * radius,
+        });
+
+        return points;
+    }
+
+    #assessDamage(roadBorders) {
+        for(const border of roadBorders) {
+            if(polysIntersect(this.polygon, border)){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- utils.js
+++ utils.js
@@ -29,7 +29,6 @@ function getIntersection(A, B, C, D) {
         const u = uTop / bottom;
         
         if(t >= 0 && t <= 1 && u >= 0 && u <= 1) {
-            console.log(t)
             return {
                 x: lerp(A.x, B.x, t),
                 y: lerp(A.y, B.y, t),
@@ -38,4 +37,23 @@ function getIntersection(A, B, C, D) {
         }
     }
     return null;
+}
+
+function polysIntersect(poly1, poly2) {
+    for(let i = 0; i < poly1.length; i++) {
+        for(let j = 0; j < poly2.length; j++) {
+            const touch = getIntersection(
+                poly1[i],
+                poly1[(i + 1) % poly1.length],
+                poly2[j],
+                poly2[(j + 1) % poly2.length],
+            );
+
+            if(touch) {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
